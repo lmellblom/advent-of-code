bc9c13e2ee066e285913251a0584f12c475878b8
Linnéa Mellblom <lmellblom@gmail.com>
2020-12-12 12:57:27 +0100

    Not the best solution but its working 🤷🏼‍♀️

--- 12dec/12dec.cs
+++ 12dec/12dec.cs
@@ -18,18 +18,16 @@ namespace adventOfCode2020
             return Math.Abs(sum1) + Math.Abs(sum2);
         }
 
-        public class Navigation
+        public class Position
         {
-            public List<string> Instructions { get; set; }
-            private int Facing { get; set; }
-            private int North { get; set; }  // N
-            private int West { get; set; }   // W
-            private int South { get; set; }  // S
-            private int East { get; set; }   // E
-            public Navigation(List<string> instructions, (int north, int south, int west, int east, int facing) starts)
-            {
-                Instructions = instructions;
+            public int Facing { get; set; }
+            public int North { get; set; }  // N
+            public int West { get; set; }   // W
+            public int South { get; set; }  // S
+            public int East { get; set; }   // E
 
+            public Position((int north, int south, int west, int east, int facing) starts)
+            {
                 // starts att
                 Facing = starts.facing;
                 North = starts.north;
@@ -38,14 +36,14 @@ namespace adventOfCode2020
                 East = starts.east;
             }
 
-            private void SetFacing(int degrees)
+            public Position(Position p)
             {
-                Facing += degrees;
-                Facing = Facing % 360;
-                if (Facing < 0)
-                {
-                    Facing += 360;
-                }
+                // starts att
+                Facing = p.Facing;
+                North = p.North;
+                West = p.West;
+                South = p.South;
+                East = p.East;
             }
 
             public char GetFacing()
@@ -68,48 +66,50 @@ namespace adventOfCode2020
                 }
             }
 
-            public int ManhattanDistance()
+            public void SetFacing(int degrees)
             {
-                var res = December12.ManhattanDistance(East, West, South, North);
-                return res;
-            }
-
-            public void Move()
-            {
-                foreach (var instruction in Instructions)
+                Facing += degrees;
+                Facing = Facing % 360;
+                if (Facing < 0)
                 {
-                    int value = Int32.Parse(Regex.Replace(instruction, @"\D", ""));
-                    char action = instruction[0];
-
-                    switch (action)
-                    {
-                        case 'F':
-                            // go forward in the direction we are facing
-                            GoForward(value);
-                            break;
-                        case 'L':
-                            SetFacing(value * -1);
-                            break;
-                        case 'R':
-                            SetFacing(value);
-                            break;
-                        case 'N':
-                            North += value;
-                            break;
-                        case 'S':
-                            South += value;
-                            break;
-                        case 'W':
-                            West += value;
-                            break;
-                        case 'E':
-                            East += value;
-                            break;
-                    }
+                    Facing += 360;
                 }
             }
 
-            private void GoForward(int value)
+            public void Rotate(int degrees)
+            {
+                degrees = degrees % 360;
+                if (degrees < 0)
+                {
+                    degrees += 360;
+                }
+
+                // flip everything depending on degrees
+                var copiedPos = new Position(this);
+                if (degrees == 90)
+                {
+                    West = copiedPos.North;
+                    North = copiedPos.East;
+                    East = copiedPos.South;
+                    South = copiedPos.West;
+                }
+                else if (degrees == 180)
+                {
+                    South = copiedPos.North;
+                    West = copiedPos.East;
+                    North = copiedPos.South;
+                    East = copiedPos.West;
+                }
+                else
+                {
+                    East = copiedPos.North;
+                    South = copiedPos.East;
+                    West = copiedPos.South;
+                    North = copiedPos.West;
+                }
+            }
+
+            public void GoForward(int value)
             {
                 var facingChar = GetFacing();
                 switch (facingChar)
@@ -128,6 +128,112 @@ namespace adventOfCode2020
                         break;
                 }
             }
+
+            public void Move(Position position, int value)
+            {
+                East += (position.East * value);
+                North += (position.North * value);
+                West += (position.West * value);
+                South += (position.South * value);
+            }
+        }
+
+        public class Navigation
+        {
+            public List<string> Instructions { get; set; }
+            public Position WayPoint { get; set; }
+            public Position Boat { get; set; }
+
+            public Navigation(List<string> instructions, (int north, int south, int west, int east, int facing) starts)
+            {
+                Instructions = instructions;
+
+                Boat = new Position(starts);
+            }
+
+            public void SetWayPointStart((int north, int south, int west, int east, int facing) starts)
+            {
+                WayPoint = new Position(starts);
+            }
+
+            public int ManhattanDistance(Position pos)
+            {
+                var res = December12.ManhattanDistance(pos.East, pos.West, pos.South, pos.North);
+                return res;
+            }
+
+            public void MoveWaypoint()
+            {
+                SetWayPointStart((1, 0, 0, 10, Boat.Facing));
+                foreach (var instruction in Instructions)
+                {
+                    int value = Int32.Parse(Regex.Replace(instruction, @"\D", ""));
+                    char action = instruction[0];
+
+                    switch (action)
+                    {
+                        case 'F':
+                            // move forward to the waypoint a number of times equal to the given value.
+                            Boat.Move(WayPoint, value);
+                            break;
+                        case 'L':
+                            // rotate the waypoint around the ship left (counter-clockwise) the given number of degrees
+                            WayPoint.Rotate(value);
+                            break;
+                        case 'R':
+                            // rotate the waypoint around the ship right (clockwise) the given number of degrees
+                            WayPoint.Rotate(value * -1);
+                            break;
+                        case 'N':
+                            WayPoint.North += value;
+                            break;
+                        case 'S':
+                            WayPoint.South += value;
+                            break;
+                        case 'W':
+                            WayPoint.West += value;
+                            break;
+                        case 'E':
+                            WayPoint.East += value;
+                            break;
+                    }
+                }
+            }
+
+            public void MoveBoat()
+            {
+                foreach (var instruction in Instructions)
+                {
+                    int value = Int32.Parse(Regex.Replace(instruction, @"\D", ""));
+                    char action = instruction[0];
+
+                    switch (action)
+                    {
+                        case 'F':
+                            // go forward in the direction we are facing
+                            Boat.GoForward(value);
+                            break;
+                        case 'L':
+                            Boat.SetFacing(value * -1);
+                            break;
+                        case 'R':
+                            Boat.SetFacing(value);
+                            break;
+                        case 'N':
+                            Boat.North += value;
+                            break;
+                        case 'S':
+                            Boat.South += value;
+                            break;
+                        case 'W':
+                            Boat.West += value;
+                            break;
+                        case 'E':
+                            Boat.East += value;
+                            break;
+                    }
+                }
+            }
         }
 
         public override bool Test()
@@ -135,8 +241,8 @@ namespace adventOfCode2020
             string filename = GetTestFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
             var shipNavigation = new Navigation(input, (0, 0, 0, 0, 90));
-            shipNavigation.Move();
-            int result = shipNavigation.ManhattanDistance();
+            shipNavigation.MoveBoat();
+            int result = shipNavigation.ManhattanDistance(shipNavigation.Boat);
             bool testSucceeded = result == 25;
             return testSucceeded;
         }
@@ -146,8 +252,8 @@ namespace adventOfCode2020
             string filename = GetFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
             var navigation = new Navigation(input, (0, 0, 0, 0, 90));
-            navigation.Move();
-            int result = navigation.ManhattanDistance();
+            navigation.MoveBoat();
+            int result = navigation.ManhattanDistance(navigation.Boat);
             return result.ToString();
         }
 
@@ -155,7 +261,10 @@ namespace adventOfCode2020
         {
             string filename = GetTestFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            bool testSucceeded = false;
+            var shipNavigation = new Navigation(input, (0, 0, 0, 0, 90));
+            shipNavigation.MoveWaypoint();
+            int result = shipNavigation.ManhattanDistance(shipNavigation.Boat);
+            bool testSucceeded = result == 286;
             return testSucceeded;
         }
 
@@ -163,7 +272,10 @@ namespace adventOfCode2020
         {
             string filename = GetFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            return "not implemented";
+            var shipNavigation = new Navigation(input, (0, 0, 0, 0, 90));
+            shipNavigation.MoveWaypoint();
+            int result = shipNavigation.ManhattanDistance(shipNavigation.Boat);
+            return result.ToString();
         }
     }
 }
