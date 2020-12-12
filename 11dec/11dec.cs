6a67e4c7cb6ba25c0b615c4eecca555ca38194da
Linnéa Mellblom <lmellblom@gmail.com>
2020-12-12 10:59:02 +0100

    solved part 2 for 11 dec

--- 11dec/11dec.cs
+++ 11dec/11dec.cs
@@ -14,25 +14,55 @@ namespace adventOfCode2020
         public static char SEAT_IS_OCCUPIED = '#';
         public static char FLOOR = '.';
 
+        public bool IsSeat(char input)
+        {
+            return input == SEAT_IS_EMPTY || input == SEAT_IS_OCCUPIED;
+        }
+
         public override bool Test()
         {
             string filename = GetTestFilename();
             var input = System.IO.File.ReadAllLines(filename).ToList();
-            int seats = HowManySeatsOccupied(input);
+            int seats = HowManySeatsOccupied(input, Rule1);
             bool testSucceeded = seats == 37;
             return testSucceeded;
         }
 
-        private int HowManySeatsOccupied(List<string> input)
+        public override string First()
         {
-            var newLayout = GetSeatLayout(input);
+            string filename = GetFilename();
+            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
+            int seats = HowManySeatsOccupied(input, Rule1);
+            return seats.ToString();
+        }
+
+        public override bool Test2()
+        {
+            string filename = GetTestFilename();
+            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
+            int seats = HowManySeatsOccupied(input, Rule2);
+            bool testSucceeded = seats == 26;
+            return testSucceeded;
+        }
+
+        public override string Second()
+        {
+            string filename = GetFilename();
+            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
+            int seats = HowManySeatsOccupied(input, Rule2);
+            return seats.ToString();
+        }
+
+        private int HowManySeatsOccupied(List<string> input, Func<List<string>, int, int, string> rule)
+        {
+            var newLayout = GetSeatLayout(input, rule);
             var newLayoutString = string.Join(' ', newLayout);
             var inputLayoutString = string.Join(' ', input);
 
             while (newLayoutString != inputLayoutString)
             {
                 input = newLayout;
-                newLayout = GetSeatLayout(input);
+                newLayout = GetSeatLayout(input, rule);
                 newLayoutString = string.Join(' ', newLayout);
                 inputLayoutString = string.Join(' ', input);
             }
@@ -41,11 +71,44 @@ namespace adventOfCode2020
             return newLayoutString.Where(seat => seat == SEAT_IS_OCCUPIED).Count();
         }
 
-        private List<string> GetSeatLayout(List<string> input)
+        private string Rule1(List<string> input, int rowIndex, int colIndex)
         {
-            // shuffle the layout, go trough all and change input into a new array?
-            List<string> newLayout = new List<string>();
+            return Rule(input, rowIndex, colIndex, 4, GetAdjacentSeats);
+        }
 
+        private string Rule2(List<string> input, int rowIndex, int colIndex)
+        {
+            return Rule(input, rowIndex, colIndex, 5, GetFirstAdjacentSeats);
+        }
+
+        private string Rule(List<string> input, int rowIndex, int colIndex, int occupiedSeatRule, Func<List<string>, int, int, string> rule)
+        {
+            char currentSeat = input[rowIndex][colIndex];
+            if (currentSeat == FLOOR)
+            {
+                return currentSeat.ToString();
+            }
+
+            string adjacentSeats = rule(input, rowIndex, colIndex);
+            int nrOfOccupiedAdjancentSeats = CountSeats(adjacentSeats);
+
+            if (currentSeat == SEAT_IS_EMPTY && nrOfOccupiedAdjancentSeats == 0)
+            {
+                return SEAT_IS_OCCUPIED.ToString();
+            }
+            else if (currentSeat == SEAT_IS_OCCUPIED && nrOfOccupiedAdjancentSeats >= occupiedSeatRule)
+            {
+                return SEAT_IS_EMPTY.ToString();
+            }
+            else
+            {
+                return currentSeat.ToString();
+            }
+        }
+
+        private List<string> GetSeatLayout(List<string> input, Func<List<string>, int, int, string> rule)
+        {
+            List<string> newLayout = new List<string>();
             for (int rowIndex = 0; rowIndex < input.Count(); rowIndex++)
             {
                 var row = input[rowIndex];
@@ -54,35 +117,13 @@ namespace adventOfCode2020
                 // go trough each letter and add to new layout since we do not want to change current
                 for (int colIndex = 0; colIndex < row.Count(); colIndex++)
                 {
-                    char currentSeat = row[colIndex];
-                    string adjacentSeats = GetAdjacentSeats(input, rowIndex, colIndex);
-                    int nrOfOccupiedAdjancentSeats = CountSeats(adjacentSeats);
-
-                    if (currentSeat == SEAT_IS_EMPTY && nrOfOccupiedAdjancentSeats == 0)
-                    {
-                        newRow += SEAT_IS_OCCUPIED;
-                    }
-                    else if (currentSeat == SEAT_IS_OCCUPIED && nrOfOccupiedAdjancentSeats >= 4)
-                    {
-                        newRow += SEAT_IS_EMPTY;
-                    }
-                    else if (currentSeat == FLOOR)
-                    {
-                        // do nothing
-                        newRow += currentSeat;
-                    }
-                    else
-                    {
-                        // do nothing
-                        newRow += currentSeat;
-                    }
+                    newRow += rule(input, rowIndex, colIndex);
                 }
 
                 newLayout.Add(newRow);
             }
 
             return newLayout;
-
         }
 
         private int CountSeats(string input)
@@ -121,28 +162,55 @@ namespace adventOfCode2020
 
             return seats;
         }
-
-        public override string First()
+        
+        private string GetFirstAdjacentSeats(List<string> input, int rowIndex, int colIndex)
         {
-            string filename = GetFilename();
-            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            int seats = HowManySeatsOccupied(input);
-            return seats.ToString();
+            string seats = "";
+
+            seats += GetFirstSeatOccurence(input, -1, 0, rowIndex, colIndex);
+            seats += GetFirstSeatOccurence(input, 1, 0, rowIndex, colIndex);
+
+            seats += GetFirstSeatOccurence(input, 0, -1, rowIndex, colIndex);
+            seats += GetFirstSeatOccurence(input, 0, 1, rowIndex, colIndex);
+
+            seats += GetFirstSeatOccurence(input, -1, -1, rowIndex, colIndex);
+            seats += GetFirstSeatOccurence(input, 1, 1, rowIndex, colIndex);
+
+            seats += GetFirstSeatOccurence(input, 1, -1, rowIndex, colIndex);
+            seats += GetFirstSeatOccurence(input, -1, 1, rowIndex, colIndex);
+            return seats;
         }
 
-        public override bool Test2()
+        // Traverse the input with the desired steps, check for forst occurence
+        private string GetFirstSeatOccurence(List<string> input, int right, int down, int startRow, int startCol)
         {
-            string filename = GetTestFilename();
-            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            bool testSucceeded = false;
-            return testSucceeded;
-        }
+            int rowIndex = startRow; // | - number of list
+            int colIndex = startCol; // - check letter
 
-        public override string Second()
-        {
-            string filename = GetFilename();
-            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            return "not implemented";
+            rowIndex += down;
+            colIndex += right;
+
+            while (rowIndex >= 0 && rowIndex < input.Count())
+            {
+                var row = input[rowIndex]; // all letters
+                if (colIndex < 0 || colIndex >= row.Count())
+                {
+                    // out of range
+                    return "";
+                }
+
+                var letter = row[colIndex];
+                if (IsSeat(letter))
+                {
+                    var output = letter.ToString();
+                    return output;
+                }
+
+                rowIndex += down;
+                colIndex += right;
+            }
+
+            return ""; // no found
         }
     }
 }
