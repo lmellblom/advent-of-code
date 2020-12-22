5d23d928d2cb8b8f5c198f84d9e1e26adcdc8d0a
Linnéa Mellblom <lmellblom@gmail.com>
2020-12-22 19:40:18 +0100

    gjorde alldeles för krånligt! men var roligt att skriva ett liten spelmekanik så kanske tog det lite långt 🤪får skriva om lite senare

--- 22dec/22dec.cs
+++ 22dec/22dec.cs
@@ -7,25 +7,46 @@ namespace adventOfCode2020
     public class December22 : AdventOfCode
     {
         public static bool PRINT = false;
-        
+
         public December22() : base(22)
         {
         }
 
         public class Player
         {
-            // Will store the cards on the players hand
-            public Queue<int> Cards { get; private set; }
-            public int PlayedCard { get; private set; }
+            public Queue<int> Cards { get; private set; } // Will store the cards on the players hand
+            public int PlayedCard { get; private set; } // Current played card from that player
             public string Name { get; set; }
             public int NrOfCards => Cards.Count();
+            public bool Won = false; // for part 2
+
+            private string _cardsString => String.Join(",", Cards.ToArray());
+            private List<string> _memory = new List<string>();
+
             public Player(string name)
             {
                 Name = name;
                 Cards = new Queue<int>();
+                _memory = new List<string>();
             }
 
-            public void RoundEnded()
+            public Player(string name, List<int> cards)
+            {
+                Name = name;
+                Cards = new Queue<int>();
+                foreach (var card in cards)
+                {
+                    Cards.Enqueue(card);
+                }
+                _memory = new List<string>();
+            }
+
+            public bool HasPlayedCardBefore()
+            {
+                return _memory.Contains(_cardsString);
+            }
+
+            public void ResetRound()
             {
                 PlayedCard = -1;
             }
@@ -37,6 +58,7 @@ namespace adventOfCode2020
 
             public int PlayCard()
             {
+                _memory.Add(_cardsString);
                 PlayedCard = Cards.Dequeue();
                 if (PRINT) Console.WriteLine($"{Name} plays: {PlayedCard}");
                 return PlayedCard;
@@ -44,15 +66,16 @@ namespace adventOfCode2020
 
             public void WonCards(List<int> cards)
             {
-                cards.Sort();
-                cards.Reverse();
-
                 foreach (var card in cards)
                 {
                     AddCard(card);
                 }
+            }
 
-                if (PRINT) Console.WriteLine($"{Name} wins the round!");
+            public void WonCards(int card1, int card2)
+            {
+                AddCard(card1);
+                AddCard(card2);
             }
 
             public void PrintCards()
@@ -72,29 +95,99 @@ namespace adventOfCode2020
                     score += (card * multiplier);
                     multiplier--;
                 }
-
                 return score;
             }
         }
 
         public class RecursiveCombat : CrabCombat
         {
-            public RecursiveCombat(List<string> input) : base(input)
+            public int SubGame = 1;
+            public RecursiveCombat(List<string> input, int gameNr = 1) : base(input, gameNr)
             {
+            }
+
+            public RecursiveCombat(List<Player> players, int gameNr = 1) : base(players, gameNr)
+            {
+                Players = players;
+                TotalNumberOfCards = Players.Sum(p => p.Cards.Count);
+            }
+
+            protected override void SetResultOfRound()
+            {
+                // CHECK IF A RECURSIVE GAME SHOULD HAPPEN
+
+                // If both players have at least as many cards remaining 
+                // in their deck as the value of the card they just drew, 
+                // the winner of the round is determined by playing a new game of Recursive Combat (see below).
+                Player playerWon;
+                bool playSubgame = Players.All(p => p.NrOfCards >= p.PlayedCard);
+                if (playSubgame)
+                {
+                    if (PRINT) Console.WriteLine("Playing a sub-game to determine the winner...");
+
+                    var playersWithNewCards = Players.Select(p => new Player(p.Name, p.Cards.ToList().Take(p.PlayedCard).ToList())).ToList();
+                    SubGame++;
+                    var newGame = new RecursiveCombat(playersWithNewCards, SubGame);
+                    var playerWonRecursiceGame = newGame.PlayGame();
+                    playerWon = Players.FirstOrDefault(p => p.Name == playerWonRecursiceGame.Name);
+                    var otherPlayer = Players.FirstOrDefault(p => p.Name != playerWonRecursiceGame.Name);
+                    // note that the winner's card might be the lower-valued of the two cards
+                    playerWon.WonCards(playerWon.PlayedCard, otherPlayer.PlayedCard);
+
+                    if (PRINT) Console.WriteLine($"...anyway, back to game {GameNr}.");
+                }
+                else
+                {
+                    // find player with the highest card!
+                    playerWon = Players.OrderBy(p => p.PlayedCard).Reverse().FirstOrDefault();
+
+                    CardsOnTable.Sort();
+                    CardsOnTable.Reverse();
+                    playerWon.WonCards(CardsOnTable);
+                }
+
+                if (PRINT) Console.WriteLine($"{playerWon.Name} wins round {Round} of game {GameNr}!");
+
+                // reset round!
+                Players.ForEach(p => p.ResetRound());
+                CardsOnTable = new List<int>();
+            }
+
+            protected override bool RoundHasEnded()
+            {
+                // if there was a previous round in this game that had exactly the same cards in the same order in the same players' decks, 
+                // the game instantly ends in a win for player 1.
+                var shouldEnd = Players.Any(p => p.HasPlayedCardBefore());
+                if (shouldEnd)
+                {
+                    if (PRINT) Console.WriteLine("Game ended! Same cards appeared..");
+                    Players.FirstOrDefault().Won = true;
+                    return true;
+                }
+
+                var player = Players.FirstOrDefault(p => p.NrOfCards == TotalNumberOfCards);
+                if (player != null)
+                {
+                    player.Won = true;
+                    return true;
+                }
+                return false;
             }
         }
 
         public class CrabCombat
         {
+            public int GameNr { get; set; }
             public List<Player> Players { get; set; }
-
             public int Round { get; set; }
+            protected int TotalNumberOfCards = 0;
+            protected List<int> CardsOnTable { get; set; }
 
-            private int TotalNumberOfCards = 0;
-
-            public CrabCombat(List<string> input)
+            public CrabCombat(List<string> input, int gameNr = 1)
             {
                 Players = new List<Player>();
+                CardsOnTable = new List<int>();
+                GameNr = gameNr;
                 // split the input on the players
                 string playerName = "";
                 foreach (var line in input)
@@ -117,55 +210,94 @@ namespace adventOfCode2020
                 }
             }
 
-            public void PlayGame()
+            public CrabCombat(List<Player> players, int gameNr = 1)
             {
+                Players = players;
+                GameNr = gameNr;
+                TotalNumberOfCards = Players.Sum(p => p.Cards.Count);
+                CardsOnTable = new List<int>();
+            }
+
+            public Player PlayGame()
+            {
+                if (PRINT) Console.WriteLine($"=== Game {GameNr} ===");
                 Round = 0;
 
                 bool playNextRound = true;
                 while (playNextRound)
                 {
-                    Round++;
-                    PlayRound();
-                    if (PRINT) Console.WriteLine("");
-
-                    // check if the round has ended?
-                    if (Players.Any(p => p.NrOfCards == TotalNumberOfCards))
+                    if (RoundHasEnded())
                     {
                         playNextRound = false;
+                        break;
+                    }
+                    else
+                    {
+                        Round++;
+                        PlayRound();
+                        if (PRINT) Console.WriteLine("");
                     }
                 }
+                var playerWon = Players.FirstOrDefault(p => p.Won);
+                if (PRINT) Console.WriteLine($"The winner of game {GameNr} is {playerWon.Name}!");
 
-                Console.WriteLine("== Post-game results ==");
-                foreach (var player in Players)
+                if (GameNr == 1)
                 {
-                    player.PrintCards();
+                    if (PRINT) Console.WriteLine("== Post-game results ==");
+                    foreach (var player in Players)
+                    {
+                        player.PrintCards();
+                    }
                 }
+                return playerWon;
             }
 
-            public int Score1()
+            protected virtual bool RoundHasEnded()
+            {
+                // check if the round has ended?
+                var player = Players.FirstOrDefault(p => p.NrOfCards == TotalNumberOfCards);
+                if (player != null)
+                {
+                    player.Won = true;
+                    return true;
+                }
+                return false;
+            }
+
+            public int Score()
             {
                 // get the player that won!
-                var playerWon = Players.FirstOrDefault(p => p.NrOfCards == TotalNumberOfCards);
+                var playerWon = Players.FirstOrDefault(p => p.Won == true);
                 var score = playerWon.GetScore();
                 return score;
             }
 
-            private void PlayRound()
+            protected virtual void PlayRound()
             {
-                List<int> cardsOnTable = new List<int>();
-
-                if (PRINT) Console.WriteLine($"-- Round {Round} --");
-                if (PRINT) Players.ForEach(p => p.PrintCards());
-                Players.ForEach(p => cardsOnTable.Add(p.PlayCard()));
-
-                // find player with the highest card!
-                var playerWon = Players.OrderBy(p => p.PlayedCard).Reverse().FirstOrDefault();
-                playerWon.WonCards(cardsOnTable);
-
-                // reset round!
-                Players.ForEach(p => p.RoundEnded());
+                PlayCards();
+                SetResultOfRound();
             }
 
+            protected virtual void SetResultOfRound()
+            {
+                // find player with the highest card!
+                var playerWon = Players.OrderBy(p => p.PlayedCard).Reverse().FirstOrDefault();
+                CardsOnTable.Sort();
+                CardsOnTable.Reverse();
+                playerWon.WonCards(CardsOnTable);
+                if (PRINT) Console.WriteLine($"{playerWon.Name} wins round {Round} of game {GameNr}!");
+
+                // reset round!
+                Players.ForEach(p => p.ResetRound());
+                CardsOnTable = new List<int>();
+            }
+
+            protected void PlayCards()
+            {
+                if (PRINT) Console.WriteLine($"-- Round {Round} (Game: {GameNr}) --");
+                if (PRINT) Players.ForEach(p => p.PrintCards());
+                Players.ForEach(p => CardsOnTable.Add(p.PlayCard()));
+            }
         }
 
         public override bool Test()
@@ -173,10 +305,10 @@ namespace adventOfCode2020
             string filename = GetTestFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
 
-            PRINT = true;
+            PRINT = false;
             CrabCombat game = new CrabCombat(input);
             game.PlayGame();
-            var result = game.Score1();
+            var result = game.Score();
 
             bool testSucceeded = result == 306;
             return testSucceeded;
@@ -186,10 +318,12 @@ namespace adventOfCode2020
         {
             string filename = GetFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
+
             PRINT = false;
             CrabCombat game = new CrabCombat(input);
             game.PlayGame();
-            var result = game.Score1();
+
+            var result = game.Score();
             return result.ToString();
         }
 
@@ -197,7 +331,18 @@ namespace adventOfCode2020
         {
             string filename = GetTestFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            bool testSucceeded = false;
+            PRINT = false;
+            RecursiveCombat game = new RecursiveCombat(input);
+            game.PlayGame();
+            var result = game.Score();
+
+            // string filename2 = GetTest2Filename();
+            // List<string> input2 = System.IO.File.ReadAllLines(filename2).ToList();
+            // RecursiveCombat game2 = new RecursiveCombat(input2);
+            // game2.PlayGame();
+            // game2.Score2();
+
+            bool testSucceeded = result == 291;
             return testSucceeded;
         }
 
@@ -205,7 +350,11 @@ namespace adventOfCode2020
         {
             string filename = GetFilename();
             List<string> input = System.IO.File.ReadAllLines(filename).ToList();
-            return "not implemented";
+            PRINT = false;
+            RecursiveCombat game = new RecursiveCombat(input);
+            game.PlayGame();
+            var result = game.Score();
+            return result.ToString();
         }
     }
 }
