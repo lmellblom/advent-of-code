4a3cf80c183de20102f88586d640b9a14292c1ef
Linnéa Mellblom <lmellblom@gmail.com>
2020-12-23 16:24:47 +0100

    part1 done. reduce to int instead of class.. part2 is hard!!

--- 23dec/23dec.cs
+++ 23dec/23dec.cs
@@ -11,34 +11,26 @@ namespace adventOfCode2020
         {
         }
 
-        public class Cup
-        {
-            public int Value { get; set; }
-            public bool IsPicked { get; set; }
-            public Cup(int value)
-            {
-                Value = value;
-                IsPicked = false;
-            }
-            public override bool Equals(object obj)
-            {
-                return obj is Cup cup &&
-                       Value == cup.Value;
-            }
-            public override int GetHashCode()
-            {
-                return HashCode.Combine(Value);
-            }
-        }
-
         public class CrabCups
         {
-            public List<Cup> CupsInCircle { get; set; }
+            public List<int> Cups { get; set; }
             public int CurrcentCupIndex { get; set; }
             public CrabCups(string cups)
             {
-                CupsInCircle = cups.Select(c => c.ToString()).Select(Int32.Parse).Select(i => new Cup(i)).ToList();
+                Cups = cups.Select(c => c.ToString()).Select(Int32.Parse).ToList();
                 CurrcentCupIndex = 0;
+            }
+
+            public CrabCups(string cups, bool reachMillion)
+            {
+                Cups = cups.Select(c => c.ToString()).Select(Int32.Parse).ToList();
+                CurrcentCupIndex = 0;
+
+                var maxValue = Cups.Max();
+                for (int i = maxValue + 1; i <= 1000000; i++)
+                {
+                    Cups.Add(i);
+                }
             }
 
             public void Play(int until = 10)
@@ -49,29 +41,26 @@ namespace adventOfCode2020
                     if (PRINT) PrintCups();
 
                     // the current cup
-                    Cup currentCup = CupsInCircle[CurrcentCupIndex];
+                    int currentCup = Cups[CurrcentCupIndex];
 
                     // pick up the three cups that are immediately clockwise of the current cup
-                    List<Cup> cupsInHand = PickupCups();
+                    List<int> cupsInHand = PickupCups();
 
                     // select a destination cup. the cup with a label equal to the current cup's label minus one
                     // can only be on the board to search. if not found lowest, go up and search from the highest again
-                    Cup destinationCup = SelectDestinationCup(currentCup.Value);
+                    int destinationCup = SelectDestinationCup(currentCup);
 
                     // insert the cup from the hands in the right position after the destination cup
-                    var destinationCupIndex = CupsInCircle.IndexOf(destinationCup);
-                    CupsInCircle.InsertRange(destinationCupIndex + 1, cupsInHand);
+                    var destinationCupIndex = Cups.IndexOf(destinationCup);
+                    Cups.InsertRange(destinationCupIndex + 1, cupsInHand);
 
                     // make sure the holden cup is at the same index as before! otherwise shift everything 
-                    while (currentCup.Value != CupsInCircle[CurrcentCupIndex].Value)
+                    while (currentCup != Cups[CurrcentCupIndex])
                     {
-                        var firstCup = CupsInCircle.FirstOrDefault();
-                        CupsInCircle.RemoveAt(0);
-                        CupsInCircle.Add(firstCup);
+                        var firstCup = Cups.FirstOrDefault();
+                        Cups.RemoveAt(0);
+                        Cups.Add(firstCup);
                     }
-
-                    // reset the picked status
-                    CupsInCircle.ForEach(c => c.IsPicked = false);
 
                     // select a new current cup, the cup which is immediately clockwise of the current cup
                     CurrcentCupIndex = GetNextCupIndex(CurrcentCupIndex);
@@ -84,52 +73,53 @@ namespace adventOfCode2020
             public string GetResult()
             {
                 var res = "";
-                var oneIndex = CupsInCircle.FindIndex(c => c.Value == 1);
+                var oneIndex = Cups.FindIndex(c => c == 1);
                 var lookIndex = GetNextCupIndex(oneIndex);
                 while (lookIndex != oneIndex)
                 {
-                    res += CupsInCircle[lookIndex].Value;
+                    res += Cups[lookIndex];
                     lookIndex = GetNextCupIndex(lookIndex);
                 }
                 return res;
             }
 
-            protected Cup SelectDestinationCup(int currentCupValue)
+            protected int SelectDestinationCup(int currentCupValue)
             {
                 // not the currenc cup!
-                var cupsToSearchIn = CupsInCircle.Where(c => c.Value != currentCupValue).ToList();
+                var cupsToSearchIn = Cups.Where(c => c != currentCupValue).ToList();
 
                 // if not found, then go up and search from the highest
-                var minValue = cupsToSearchIn.Min(c => c.Value);
-                var maxValue = cupsToSearchIn.Max(c => c.Value);
+                var minValue = cupsToSearchIn.Min();
+                var maxValue = cupsToSearchIn.Max();
 
                 var valueToSearchFor = currentCupValue - 1;
                 valueToSearchFor = valueToSearchFor < minValue ? maxValue : valueToSearchFor;
 
-                var itemFound = cupsToSearchIn.FirstOrDefault(c => c.Value == valueToSearchFor);
-                while (itemFound == null)
+                var itemFoundIndex = cupsToSearchIn.FindIndex(c => c == valueToSearchFor);
+                while (itemFoundIndex == -1)
                 {
                     valueToSearchFor--;
-                    itemFound = cupsToSearchIn.FirstOrDefault(c => c.Value == valueToSearchFor);
+                    itemFoundIndex = cupsToSearchIn.FindIndex(c => c == valueToSearchFor);
                 }
 
-                if (PRINT) Console.WriteLine($"destination: {itemFound.Value}");
+                var itemFound = cupsToSearchIn[itemFoundIndex];
+
+                if (PRINT) Console.WriteLine($"destination: {itemFound}");
 
                 return itemFound;
             }
 
-            protected List<Cup> PickupCups()
+            protected List<int> PickupCups()
             {
                 var indexesToPick = new List<int>() { 0, 1, 2 }.Select(i => GetNextCupIndex(CurrcentCupIndex + i)).ToList();
-                var cupsToPickUp = indexesToPick.Select(i => CupsInCircle[i]).ToList();
-                indexesToPick.ForEach(i => CupsInCircle[i].IsPicked = true);
-                if (PRINT) Console.WriteLine($"pick up: {String.Join(", ", cupsToPickUp.Select(s => s.Value))}");
+                var cupsToPickUp = indexesToPick.Select(i => Cups[i]).ToList();
+                if (PRINT) Console.WriteLine($"pick up: {String.Join(", ", cupsToPickUp)}");
 
                 // remove the cups from the circle
                 foreach (var cupToRemove in cupsToPickUp)
                 {
-                    var index = CupsInCircle.IndexOf(cupToRemove);
-                    CupsInCircle.RemoveAt(index);
+                    var index = Cups.IndexOf(cupToRemove);
+                    Cups.RemoveAt(index);
                 }
 
                 return cupsToPickUp;
@@ -138,9 +128,9 @@ namespace adventOfCode2020
             protected void PrintCups()
             {
                 var strings = new List<string>();
-                for (int i = 0; i < CupsInCircle.Count(); i++)
+                for (int i = 0; i < Cups.Count(); i++)
                 {
-                    var cup = CurrcentCupIndex == i ? $"({CupsInCircle[i].Value})" : $"{CupsInCircle[i].Value}";
+                    var cup = CurrcentCupIndex == i ? $"({Cups[i]})" : $"{Cups[i]}";
                     strings.Add(cup);
                 }
                 Console.WriteLine($"cups: {String.Join(" ", strings)}");
@@ -148,7 +138,7 @@ namespace adventOfCode2020
 
             public int GetNextCupIndex(int currcentCupIndex)
             {
-                var totalCups = CupsInCircle.Count();
+                var totalCups = Cups.Count();
                 currcentCupIndex++;
                 if (currcentCupIndex >= totalCups)
                 {
@@ -183,8 +173,10 @@ namespace adventOfCode2020
 
         public override bool Test2()
         {
-            string filename = GetTestFilename();
-            List<string> input = System.IO.File.ReadAllLines(filename).ToList();
+            var game = new CrabCups("712643589", true);
+            PRINT = false;
+            game.Play(10000000);
+
             bool testSucceeded = false;
             return testSucceeded;
         }
