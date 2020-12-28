1a756591feb66a3e0746271aa7ae3ba88acdddd5
Linnéa Mellblom <lmellblom@gmail.com>
2020-12-28 10:51:18 +0100

    empty shell for every day in dec

---
+++ 2020/D10/Problem.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace adventOfCode.Y2020.D10 
+{
+    [CodeName("Adapter Array")]      
+    public class AoC : IAdventOfCode 
+    {
+
+        public Result First(List<string> input) 
+        {
+            return new Result("not implemented");
+        }
+
+        public Result Second(List<string> input) 
+        {
+            return new Result("not implemented");
+        }
+
+        public TestResult Test(List<string> input) 
+        {
+            var value = "-";
+            var expected = "";
+            bool succeded = value == expected;
+            return new TestResult(succeded, expected, value);
+        }
+
+        public TestResult Test2(List<string> input) 
+        {
+            var value = "-";
+            var expected = "";
+            bool succeded = value == expected;
+            return new TestResult(succeded, expected, value);
+        }
+    }
+}
