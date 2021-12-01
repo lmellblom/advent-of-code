7743ebe7af8abf8f1a7bdf2784d17198792ce423
Linnéa Mellblom <linnea.mellblom@gaia.se>
2021-12-01 21:24:00 +0100

    Sonar Sweep - 1 dec

---
+++ 2021/common/helpers.py
@@ -0,0 +1,2 @@
+def convertListToInt(input: list) -> list :
+    return list(map(int, input))
--- 2021/days/day1/day.py
+++ 2021/days/day1/day.py
@@ -1,9 +1,51 @@
-from common.adventBase import AdventBase
+from common import adventBase
+from common import helpers 
 
-class Day1(AdventBase):
+class Day1(adventBase.AdventBase):
     def __init__(self) -> None:
-        super().__init__('Dag1 - TBD', 1, __file__)
+        super().__init__('Sonar Sweep', 1, __file__)
 
-    def first(self):
-        input = self.readInputFile()
-        print(input)
+    # --- First ---
+    def test(self, input: list)-> adventBase.TestResult:
+        value = self.__first(input)
+        return adventBase.TestResult(7, value)
+
+    def first(self, input: list) -> adventBase.Result:
+        value = self.__first(input)
+        return adventBase.Result(value)
+
+    def __first(self, input: list):
+        intList = helpers.convertListToInt(input)
+        value = self.__countDepthIncreases(intList)
+        return value
+
+    # --- Second ---
+    def test2(self, input: list) -> adventBase.TestResult:
+        value = self.__second(input)
+        return adventBase.TestResult(5, value)
+
+    def second(self, input: list) -> adventBase.Result:
+        value = self.__second(input)
+        return adventBase.Result(value)
+
+    def __second(self, input: list):
+        intList = helpers.convertListToInt(input)
+        slidingWindow = self.__createSlidingWindow(intList)
+        value = self.__countDepthIncreases(slidingWindow)
+        return value
+
+    # Helper methods for calculating
+    def __createSlidingWindow(self, values: list) -> list:
+        newValues = []
+        for prev, current, next in zip(values, values[1:], values[2:]):
+            sum = prev + current + next
+            newValues.append(sum)
+        return newValues
+
+    def __countDepthIncreases(self, values) -> int:
+        increases = 0
+        for prev, current in zip(values, values[1:]):
+            if (current > prev):
+                increases += 1
+        return increases
+    
